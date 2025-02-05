1b931a0712be2975d1f8aecb9302adda620556fb
OmondiEricDev <ericomondidev@gmail.com>
2025-02-05 17:23:00 +0900

    added user auth

---
+++ backend/app/main.py
@@ -0,0 +1,51 @@
+# from typing import Union
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+from app.utils.auth import build_strava_auth_url, get_strava_access_token
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "world"}
+
+@app.get("/auth/login")
+async def login():
+    """ Redirect users to Strava's 3rd party app authorization page
+    """
+    auth_url = build_strava_auth_url()
+    return RedirectResponse(url=auth_url)
+
+@app.get("/auth/callback")
+async def callback(code: str):
+    """ Called by Strava in response to authorization request
+
+    Args:
+        code (str): Strava provided authorization code needed to obtain access token
+    """
+    token_data = await get_strava_access_token(code)
+    if not token_data:
+        raise HTTPException(status_code=400, detail="Failed to retrieve access token!!!")
+    
+    # TODO: store access token in redis database and associate it with specific athlete
+    # ** Example token_data response
+    #     {
+    #       "token_type": "Bearer",
+    #       "expires_at": 1568775134,
+    #       "expires_in": 21600,
+    #       "refresh_token": "e5n567567...",
+    #       "access_token": "a4b945687g...",
+    #       "athlete": {
+    #           #{summary athlete representation}
+    #       }
+    #      }
+    #
+    # ** NOTE: token expiry details
+    # ** NOTE: what kind of athlete info is in the summary???
+    
+    access_token = token_data.get("access_token")
+    athlete = token_data.get("athlete")
+    
+    return {"access_token": access_token,
+            "athlete": athlete}
---
+++ backend/app/utils/auth.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from dotenv import load_dotenv
+from fastapi import HTTPException
+
+# Load environment variables
+load_dotenv()
+
+# Strava OAuth URLs and parameters
+STRAVA_AUTH_URL = "https://www.strava.com/oauth/authorize"
+STRAVA_TOKEN_URL = "https://www.strava.com/oauth/token"
+APPROVAL_PROMPT = "auto"
+AUTH_SCOPE = "read"
+
+# Get environment variables
+CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("STRAVA_REDIRECT_URI")
+
+"""Build and return the auth URL
+"""
+def build_strava_auth_url() -> str:
+    return (
+        f"{STRAVA_AUTH_URL}?"
+        f"client_id={CLIENT_ID}&"
+        f"redirect_uri={REDIRECT_URI}&"
+        f"response_type=code&"
+        f"approval_prompt={APPROVAL_PROMPT}&"
+        f"scope={AUTH_SCOPE}"
+    )
+
+"""_summary_
+"""
+async def get_strava_access_token(code: str):
+    data={
+                "client_id": CLIENT_ID,
+                "client_secret": CLIENT_SECRET,
+                "code": code,
+                "grant_type": "authorization_code"
+            }
+    try:
+        response = requests.post(STRAVA_TOKEN_URL, data)
+        response.raise_for_status() # Raises an error if any
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=400,
+                            detail=f"Failed to fetch access token: {e}")
